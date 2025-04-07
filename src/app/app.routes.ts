061d9fb7f7496aab7f767fce2d008a61d7295288
kunalArya1 <kunalkrraj220@gmail.com>
2025-04-07 23:26:25 +0530

    App Crearted and Home Page added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => {
+      return import('./Page/home/home.component').then((m) => m.HomeComponent);
+    },
+  },
+  {
+    path: 'login',
+    loadComponent: () => {
+      return import('./Page/login/login.component').then(
+        (m) => m.LoginComponent
+      );
+    },
+  },
+  {
+    path: 'register',
+    loadComponent: () => {
+      return import('./Page/register/register.component').then(
+        (m) => m.RegisterComponent
+      );
+    },
+  },
+];
